127adfd585677f02a5861d121b757c8902410d2d
Marcio Nicolau <marcionicolau@me.com>
2019-05-08 16:36:12 -0300

    add todos reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <Fragment>
+        <h1>Hello World</h1>
+        <TodoList />
+      </Fragment>
     </Provider>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => (
+  <ul>
+    <li>Fazer café</li>
+    <li>Estudar React</li>
+    <li>Entrar na comunidade</li>
+  </ul>
+);
+
+export default TodoList;
